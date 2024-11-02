48578eb53665fecf78d84796125c929b3175b8e6
samandar-hodiev <samandarkhodiev04@gmail.com>
2024-11-03 03:40:17 +0500

    Global searcher added to this project

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@
 
 const data = movies.splice(0, 102);
 const cardsWrapper = $("#cards");
-console.log(cards);
+// console.log(cards);
+const globalSearch = $("#search");
+// console.log(globalSearch);
+const searchBtn = $("#search-btn");
 
 
 
@@ -28,50 +31,56 @@ const normalize = data.map((el)=>{
 
 
 
+// ----------------------------------- rendering data ----------------------------------- //
 function renderData(data){
-    if(data.length){
-        data?.forEach((element) => {
-            const card = createElement('div', 'card', `
-                <img src="${element.imgMax}" alt="foto">
-                        <div class="card-body">
-                            <h2 class="font-bold">${element.title}</h2>
-                            <ul class="mb-4">
-                                <li>
-                                    <strong>Year:</strong>
-                                    <span>${element.year}</span>
-                                </li>
-                                <li>
-                                    <strong>Rating:</strong>
-                                    <span>${element.rating}</span>
-                                </li>
-                                <li>
-                                    <strong>Language:</strong>
-                                    <span>${element.language}</span>
-                                </li>
-
-                                <li>
-                                    <strong>Genre:</strong>
-                                    <span >${element.genres}</span>
-                                </li>
-                                <li>
-                                    <strong>Runtime:</strong>
-                                    <span>${element.time}</span>
-                                </li>
-                            </ul>
-                            <div class="flex justify-between btns">
-                                <button>
-                                  Read more
-                                </button>
-                                <button>
-                                    <i class="bi bi-heart-fill"></i>
-                                </button>
+    cardsWrapper.innerHTML = `<i class="loader"></i>`;
+    setTimeout(()=>{
+        if(data.length){
+            cardsWrapper.innerHTML = " ";
+            data?.forEach((element) => {
+                const card = createElement('div', 'card', `
+                    <img src="${element.imgMax}" alt="foto">
+                            <div class="card-body">
+                                <h2 class="font-bold">${element.title}</h2>
+                                <ul class="mb-4">
+                                    <li>
+                                        <strong>Year:</strong>
+                                        <span>${element.year}</span>
+                                    </li>
+                                    <li>
+                                        <strong>Rating:</strong>
+                                        <span>${element.rating}</span>
+                                    </li>
+                                    <li>
+                                        <strong>Language:</strong>
+                                        <span>${element.language}</span>
+                                    </li>
+    
+                                    <li>
+                                        <strong>Genre:</strong>
+                                        <span >${element.genres}</span>
+                                    </li>
+                                    <li>
+                                        <strong>Runtime:</strong>
+                                        <span>${element.time}</span>
+                                    </li>
+                                </ul>
+                                <div class="flex justify-between btns">
+                                    <button>
+                                      Read more
+                                    </button>
+                                    <button id="like-button">
+                                        <i class="bi bi-heart-fill" id="heartButton"></i>
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                `);
-                
-            cardsWrapper.appendChild(card);
-        });
-    };
+                    `);
+                cardsWrapper.appendChild(card);
+            });
+        }else{
+            cardsWrapper.innerHTML = '<h1 class="text-2xl">NOT FOUND!</h1>'
+        }
+    },2000)
 };
 renderData(normalize);
 
@@ -79,29 +88,26 @@ renderData(normalize);
 
 
 
+// ----------------------------------- Global search ----------------------------------- //
 
+globalSearch.addEventListener('keyup', (e)=>{
+    const filteredData = normalize.filter((el) => el.title.toLowerCase().includes(e.target.value.toLowerCase()));
 
-
-
-
-
-
-
-
-
-
-
+    searchBtn.addEventListener('click',()=>{
+        renderData(filteredData);
+    })
+});
 
 
 
 
 
 // ----------------------------------- like button ----------------------------------- //
-const likeButton = $("#like-button");
-const heartButton = $("#heartButton");
+// const likeButton = $("#like-button");
+// const heartButton = $("#heartButton");
 
 
-likeButton.addEventListener('click', ()=>{
-    // heartButton.classList.toggle('heart-red');
-    heartButton.style='color:red;'
-})
+// likeButton.addEventListener('click', ()=>{
+//     // heartButton.classList.toggle('heart-red');
+//     // heartButton.style='color:red;'
+// })
--- readme.md
+++ readme.md
@@ -1,10 +1,14 @@
 
+# [LIVEPROJECT](https://super-cranachan-4769a0.netlify.app/)
 
 
 
 
-
-
-
+- estimated UI
 
 ![alt text](./assets/images/image.png)
+
+
+- Data normalize
+- Data rendering 
+- Global search 
